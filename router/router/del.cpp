23da8e99262a4c05d88f31bb5bdcf7cf1fb14df5
2455138283 <2455138283@qq.com>
2018-11-27 17:01:39 +0800

    del

---
+++ router/router/del.cpp
@@ -0,0 +1,36 @@
+#include "del.h"
+
+void DeleteVertex()
+{
+	int n, num, i, j;
+	num = len;
+	cout << "当前节点数目为：" << num << endl;
+	cout << "请输入删除节点名：";
+	cin >> n;
+	n -= 1;
+	for (i = 0; i < num; ++i) {//列的移动
+		for (j = n; j < num; j++) {
+			chart[i][j] = chart[i][j + 1];
+		}
+	}
+	for (i = n; i < num; i++) {//行的移动
+		for (j = 0; j < num; j++) {
+			chart[i][j] = chart[i + 1][j];
+		}
+	}
+	len = len - 1;
+
+}
+void DeleteEdge()
+{
+	int st, end;
+	cout << "请输入边的任意一个节点：";
+	cin >> st;
+	cout << "请输入另一节点值：";
+	cin >> end;
+	--st;
+	--end;
+	cout << "删除成功" << endl;
+	chart[st][end] = 100;
+	chart[end][st] = chart[st][end];
+}
